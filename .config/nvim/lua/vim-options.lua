bfbc3d6649d79b4cd5bff3fb5115b6eb15df673e
aryan <85390033+theredditbandit@users.noreply.github.com>
2024-06-03 07:49:12 +0530

    migrate window switching and deleting words in insert mode to use alt

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -10,7 +10,17 @@ vim.cmd("set incsearch")
 vim.cmd("set ignorecase")
 vim.g.neovide_transparency = 0.99
 
--- Run the 'gofumpt' command on saving a Go file
+-- remap <C-ww> and <C-w> to use alt.
+-- Map <A-w> to start window switching in normal mode
+vim.api.nvim_set_keymap('n', '<A-w>', '<C-w>', { noremap = true, silent = true })
+-- Map <A-w><A-w> to switch windows in normal mode
+vim.api.nvim_set_keymap('n', '<A-w><A-w>', '<C-w>w', { noremap = true, silent = true })
+-- Remap <A-w> to delete a word in insert mode
+vim.api.nvim_set_keymap('i', '<A-w>', '<C-w>', { noremap = true, silent = true }) -- Run the 'gofumpt' command on saving a Go file
+
+
+
+
 -- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 --     pattern = "*.go",
 --     callback = function()
