2157ab6752b7c44d7e7936b2b9a2f2b474d3a818
aryan <85390033+theredditbandit@users.noreply.github.com>
2024-06-03 08:17:04 +0530

    migrate a bunch of more commands to use alt instead of ctrl

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -17,9 +17,9 @@ vim.api.nvim_set_keymap('n', '<A-w>', '<C-w>', { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', '<A-w><A-w>', '<C-w>w', { noremap = true, silent = true })
 -- Remap <A-w> to delete a word in insert mode
 vim.api.nvim_set_keymap('i', '<A-w>', '<C-w>', { noremap = true, silent = true }) -- Run the 'gofumpt' command on saving a Go file
-
-
-
+vim.api.nvim_set_keymap('i', '<A-e>', '<C-w>', { noremap = true, silent = true }) -- Run the 'gofumpt' command on saving a Go file
+-- Remap <A-l> to clear the screen (redraw)
+vim.api.nvim_set_keymap('n', '<A-l>', '<C-l>', { noremap = true, silent = true })
 
 -- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 --     pattern = "*.go",
